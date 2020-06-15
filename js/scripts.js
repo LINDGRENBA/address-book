392e5b12d331e0c8b7dda0a8feda70254f681218
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-15 10:58:42 -0700

    Add contact3 and contact4 to scripts.js.
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ function AddressBook () {
 }
 
 AddressBook.prototype.addContact = function (contact){
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
@@ -24,8 +25,18 @@ Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 
-let addressBook = new AddressBook(); //Adds a new object for #1
+let addressBook = new AddressBook(); //Adds a new object for #1 --> the new address book = addressBook
 let contact = new Contact("Ada", "Lovelace", "503-555-0100"); //Adds a new object for #2
 let contact2 = new Contact("Grace", "Hopper", "503-555-0199"); //Adds a new object for #2
+let contact3 = new Contact("Mars", "Lindgren", "360-756-6999");
 addressBook.addContact(contact); //takes the new object for #2 and puts it into object #1 using obejct #1's method add contact
 addressBook.addContact(contact2); //""
+
+addressBook.addContact(contact3);
+console.log(contact3);
+let contact4 = new Contact ("Mary", "Rosinski", "827-343-2324");
+addressBook.addContact(contact4);
+console.log(contact4);
+
+
+
