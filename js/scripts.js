1b55e3d1de03b339fef1d1a3279f308ddfea1454
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-16 10:49:32 -0700

    Update styles.css to hide #show-contact, add initial user interface to scripts.js, refactor assign ID custom method in business logic.
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function AddressBook () {
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function (contact){
+AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
-AddressBook.prototype.assignId = function(contact) {
+AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findContact = function (id){
-  for (let i = 0; i<this.contacts.length; i++) {
-    if (this.contacts[i]){
-    if(this.contacts[i].id == id){
-      return this.contacts[i]
+AddressBook.prototype.findContact = function(id) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i]
       }
     }
-  };
+  }
   return false; 
 }
 
@@ -48,14 +48,14 @@ AddressBook.prototype.deleteContact = function (id) {
 //   return false; 
 // }
 
-AddressBook.prototype.addEmergencyContactAll = function () {
+// AddressBook.prototype.addEmergencyContactAll = function () {
   
-  for (let i = 0; i <this.contacts.length; i ++) {
-    if (this.contacts[i]){
-    this.contacts[i].emergencyContact = " ";
-    }
-  };
-}
+//   for (let i = 0; i <this.contacts.length; i ++) {
+//     if (this.contacts[i]){
+//     this.contacts[i].emergencyContact = " ";
+//     }
+//   };
+// }
 
 //Business Logic for Contacts (#2)-----
 function Contact (firstName, lastName, phoneNumber) {
@@ -64,31 +64,55 @@ function Contact (firstName, lastName, phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-Contact.prototype.fullName = function () {
+Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-Contact.prototype.update = function (favoriteColor) {
-  return this.favoriteColor = favoriteColor; 
-  // return this.favoriteColor;
-}
-Contact.prototype.newFirstName = function (newFirstName) {
-  return this.firstName = newFirstName;
+// Contact.prototype.update = function (favoriteColor) {
+//   return this.favoriteColor = favoriteColor; 
+//   // return this.favoriteColor;
+// }
+
+Contact.prototype.newFirstName = function(newFirstName) {
+  this.firstName = newFirstName;
 }
 
-let addressBook = new AddressBook(); //Adds a new object for #1 --> the new address book = addressBook
-let contact = new Contact("Ada", "Lovelace", "503-555-0100"); //Adds a new object for #2
-let contact2 = new Contact("Grace", "Hopper", "503-555-0199"); //Adds a new object for #2
-let contact3 = new Contact("Mars", "Lindgren", "360-756-6999");
-addressBook.addContact(contact); //takes the new object for #2 and puts it into object #1 using obejct #1's method add contact
-addressBook.addContact(contact2); //""
+//user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    let ourAddressBook = new AddressBook();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    ourAddressBook.addContact(newContact);
+  })
+})
 
-addressBook.addContact(contact3);
-console.log(contact3);
-let contact4 = new Contact ("Mary", "Rosinski", "827-343-2324");
-addressBook.addContact(contact4);
-console.log(contact4);
-console.log (contact2);
+
+
+
+
+
+
+
+
+
+//Code Written on Monday with no User Interface or form input
+// let addressBook = new AddressBook(); //Adds a new object for #1 --> the new address book = addressBook
+// let contact = new Contact("Ada", "Lovelace", "503-555-0100"); //Adds a new object for #2
+// let contact2 = new Contact("Grace", "Hopper", "503-555-0199"); //Adds a new object for #2
+// let contact3 = new Contact("Mars", "Lindgren", "360-756-6999");
+// addressBook.addContact(contact); //takes the new object for #2 and puts it into object #1 using obejct #1's method add contact
+// addressBook.addContact(contact2); //""
+
+// addressBook.addContact(contact3);
+// console.log(contact3);
+// let contact4 = new Contact ("Mary", "Rosinski", "827-343-2324");
+// addressBook.addContact(contact4);
+// console.log(contact4);
+// console.log (contact2);
 
 
 /*
@@ -97,9 +121,9 @@ console.log(contact2);
 let favoriteColor = "blue";
 addressBook.update(contact2);*/
 
-contact2.update("blue");
-console.log(contact2);
-contact2.newFirstName("Betty");
-console.log(contact2);
-addressBook.addEmergencyContactAll();
-addressBook.findEmptyEmergencyContact();
+// contact2.update("blue");
+// console.log(contact2);
+// contact2.newFirstName("Betty");
+// console.log(contact2);
+// addressBook.addEmergencyContactAll();
+// addressBook.findEmptyEmergencyContact();
